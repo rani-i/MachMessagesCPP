f2f8424050253a12c021c6d2c52d321688b571b5
Rani <25133607+rani-i@users.noreply.github.com>
2019-03-05 17:39:00 +0200

    Updated test

--- test/MachService.mm
+++ test/MachService.mm
@@ -19,6 +19,18 @@ void MachService::setupMachService()
 // Based on the PoC - https://github.com/rani-i/bluetoothdPoC/blob/master/bluetoothdPoC/main.m
 void MachService::playground()
 {
-    MachMessage machMessage = MachMessage(3+0xFA300, 0x113);
+    MachMessage machMessage = MachMessage(3+0xFA300, 0x1513);
+
+    typedef struct msgData_t {
+        char extraData[0x48 - sizeof(mach_msg_header_t)];
+    } msgData;
+    msgData messageData = msgData();
+
+    MachMessageData machMessageData = MachMessageData(&messageData, sizeof(msgData));
+
+    machMessage.addData(machMessageData);
+
+    machMessage.sendMessage(*this->machConnection);
+
 
 }
